3e2459622199c2d2325d8dcdb6529f13244d5752
sethWorthylake <76188592+sethWorthylake@users.noreply.github.com>
2021-02-22 22:58:01 -0800

    Update Bluetooth_Control.h

--- BuzzBox/BuzzBox/Bluetooth_Control.h
+++ BuzzBox/BuzzBox/Bluetooth_Control.h
@@ -1,14 +1,30 @@
 #pragma once
-#include "BluetoothModule.h"
+#include <TimeInterrupt.h>
+#include "Data.h"
+
+#define TxD 18
+#define RxD 19	//maybe need to take out to larger proj
+
+static int stop; // interupt will tell bluetooth to stop listening
+void setStop();
+void clearStop();
+
+
 class Bluetooth_Control
 {
 private:
-	BluetoothModule m_bluetooth_module;
 
 public:
+
 	Bluetooth_Control();
 	~Bluetooth_Control();
-	void Pair_Device();
-	void Transmit_Data();
+	void Setup();
+	void turnOn();
+	void turnOff();
+	int readyToSend(Data info);	// Will wait for the code, then send the data	
+	void sending(Data info);
+	int available();
+	
+	// Need to disable ISRs when calling this, then re enable after
+	
 };
-
